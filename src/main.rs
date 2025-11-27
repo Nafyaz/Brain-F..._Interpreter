37b350da9cb7067659fb15fa389c84859ebe3f9e
Nafis Faiyaz <nafis.faiyaz.5@gmail.com>
2025-11-28 00:37:43 +0600

    Added Text to BrainF

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).expect("Failed to read line");
+
+    for c in input.chars() {
+        let n = c as u32;
+        let sq_low = n.isqrt();
+        let sq_high = n / sq_low;
+        let rem = n % (sq_low*sq_high);
+
+        let answer = "+".repeat(sq_low as usize) + "[>" + "+".repeat(sq_high as usize).as_str() + "<-]>" + "+".repeat(rem as usize).as_str() + ".[-]<";
+        println!("{}", answer);
+    }
 }
