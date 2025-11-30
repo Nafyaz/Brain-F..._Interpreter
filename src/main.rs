3658120a995b8ff751d70c66d942d1547bbe2a09
Nafis Faiyaz <nafis.faiyaz.5@gmail.com>
2025-12-01 00:47:17 +0600

    Added clippy

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,29 @@
+#![warn(
+    clippy::all,
+    clippy::restriction,
+    clippy::pedantic,
+    clippy::nursery,
+    clippy::cargo
+)]
+
 use std::io;
 
 fn main() {
     let mut input = String::new();
-    io::stdin().read_line(&mut input).expect("Failed to read line");
+    io::stdin().read_line(&mut input).unwrap();
 
     for c in input.chars() {
         let n = c as u32;
         let sq_low = n.isqrt();
         let sq_high = n / sq_low;
-        let rem = n % (sq_low*sq_high);
+        let rem = n % (sq_low * sq_high);
 
-        let answer = "+".repeat(sq_low as usize) + "[>" + "+".repeat(sq_high as usize).as_str() + "<-]>" + "+".repeat(rem as usize).as_str() + ".[-]<";
+        let answer = "+".repeat(sq_low as usize)
+            + "[>"
+            + "+".repeat(sq_high as usize).as_str()
+            + "<-]>"
+            + "+".repeat(rem as usize).as_str()
+            + ".[-]<";
         println!("{}", answer);
     }
 }
